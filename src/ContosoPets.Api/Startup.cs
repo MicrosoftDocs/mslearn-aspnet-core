7126044ea3b8c2775795077b45ebbc13f16f5d93
Scott Addie <scott.addie@microsoft.com>
2019-03-07 17:19:18 -0600

    Updates to Api project

--- src/ContosoPets.Api/Startup.cs
+++ src/ContosoPets.Api/Startup.cs
@@ -20,6 +20,8 @@ namespace ContosoPets.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // Replace this comment with the SqlConnectionStringBuilder code
+
             services.AddDbContext<ContosoPetsContext>(options =>
                 options.UseInMemoryDatabase("ContosoPets"));
 
