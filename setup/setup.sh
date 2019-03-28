59a2541e664853928f65f91c9022555c25832e1e
Cam Soper <cam.soper@outlook.com>
2019-03-27 19:52:28 -0500

    Build App Insights instance, build remember script
    Co-Authored-By: Scott Addie <scott.addie@microsoft.com>

--- setup/setup.sh
+++ setup/setup.sh
@@ -12,12 +12,19 @@ declare gitRepoWorkingDirectory=$srcWorkingDirectory/ContosoPets.Api
 declare sqlServerName=sql$instanceId
 declare sqlHostName=$sqlServerName.database.windows.net
 declare sqlUsername=SqlUser
-declare sqlPassword=Pass.$RANDOM
+declare sqlPassword=Pass.$RANDOM.word
 declare databaseName=ContosoPets
 declare sqlConnectionString="Data Source=$sqlServerName.database.windows.net;Initial Catalog=$databaseName;Connect Timeout=30;Encrypt=True;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False"
 declare resourceGroupName=EfCoreModule
 
+declare appInsightsName=ai$instanceId
+declare subscriptionId=$(az account show --query id --output tsv)
+
+declare apiKeyTempFile='apiKey.temp'
+declare appIdTempFile='appId.temp'
+declare instrumentationKeyTempFile='instrumentationKey.temp'
 declare connectFile='connect.txt'
+declare variableScript='remember.sh'
 
 # Functions
 setAzureCliDefaults() {
@@ -71,9 +78,9 @@ downloadAndBuild() {
     echo $'\033[0;37mBuilt code!'
 }
 
-showResults() {
+writeResultsFile() {
     connectInfo=$'\n'
-    connectInfo+=$'\033[1;32m\033[4mConnection Info\033[0;37m (view again by running: \033[1;37mcat ~/connect.txt \033[0;37m)'
+    connectInfo+=$'\033[1;32m\033[4mConnection Info\033[0;37m (view again by running: \033[1;37m. ~/remember.sh \033[0;37m)'
     connectInfo+=$'\n'
 
     # db connection
@@ -93,14 +100,73 @@ showResults() {
     connectInfo+=$sqlPassword
     connectInfo+=$'\n'
 
+    # App Insights Instrumentation Key
+    connectInfo+=$'\033[1;35mApplication Insights Instrumentation Key: \033[0;37m'
+    connectInfo+=$(cat ~/$instrumentationKeyTempFile)
+    connectInfo+=$'\n'
+
+    # App Insights App ID
+    connectInfo+=$'\033[1;35mApplicationInsights App ID: \033[0;37m'
+    connectInfo+=$(cat ~/$appIdTempFile)
+    connectInfo+=$'\n'
+
+    # App Insights API Key
+    connectInfo+=$'\033[1;35mApplication Insights API Key: \033[0;37m'
+    connectInfo+=$(cat ~/$apiKeyTempFile)
+    connectInfo+=$'\n'
 
     # Set to purple for drawing .NET Bot
-    connectInfo+=$'\033[1;35m'
+    #connectInfo+=$'\033[1;35m'
 
-    echo $'Done!\n\n'
-    ~/dotnetsay/dotnetsay $'\n\033[1;37mYour environment is ready!\n\033[0;37mI set up some \033[1;34mAzure\033[0;37m resources and downloaded the code you\'ll need.\n\033[1;35mYou can find your connection information below.\033[1;35m'
     echo "$connectInfo" > ~/$connectFile
-    cat ~/$connectFile
+}
+
+# Write variables script
+writeRememberScript() {
+    text="#!/bin/bash"
+    text+=$'\n'
+    text+="declare gitUrl=$gitUrl"
+    text+=$'\n'
+    text+="declare gitBranch=$gitBranch"
+    text+=$'\n'
+    text+="declare srcWorkingDirectory=$srcWorkingDirectory"
+    text+=$'\n'
+    text+="declare gitRepoWorkingDirectory=$gitRepoWorkingDirectory"
+    text+=$'\n'
+    text+="declare sqlServerName=$sqlServerName"
+    text+=$'\n'
+    text+="declare sqlHostName=$sqlHostName"
+    text+=$'\n'
+    text+="declare sqlUsername=$sqlUsername"
+    text+=$'\n'
+    text+="declare sqlPassword=$sqlPassword"
+    text+=$'\n'
+    text+="declare databaseName=$databaseName"
+    text+=$'\n'
+    text+="declare sqlConnectionString=\"$sqlConnectionString\""
+    text+=$'\n'
+    text+="declare resourceGroupName=$resourceGroupName"
+    text+=$'\n'
+    text+="declare appInsightsName=$appInsightsName"
+    text+=$'\n'
+    text+="declare subscriptionId=$subscriptionId"
+    text+=$'\n'
+    text+="declare apiKey=$(cat ~/$apiKeyTempFile)"
+    text+=$'\n'
+    text+="declare appId=$(cat ~/$appIdTempFile)"
+    text+=$'\n'
+    text+="declare instrumentationKey=$(cat ~/$instrumentationKeyTempFile)"
+    text+=$'\n'
+    text+="cat ~/$connectFile"
+
+    echo "$text" > ~/$variableScript
+    chmod 755 ~/$variableScript
+}
+
+cleanupTempFiles() {
+    rm ~/$apiKeyTempFile
+    rm ~/$appIdTempFile
+    rm ~/$instrumentationKeyTempFile
 }
 
 # Provision Azure Resource Group
@@ -139,15 +205,58 @@ provisionDatabase() {
     echo "Provisioned Azure SQL Database!"
 }
 
+provisionAppInsights() {
+    echo "Provisioning Azure Monitor Application Insights..."
+
+    appInsightsDetails=$(az resource create \
+        --resource-type microsoft.insights/components \
+        --name $appInsightsName \
+        --is-full-object \
+        --properties '{"kind":"web","location":"southcentralus","properties":{"Application_Type":"web"}}')
+ 
+    # Create an API Key for App Insights
+    # There is no Az CLI command for this, so we must use the REST API.
+    token=$(az account get-access-token --output tsv --query accessToken)
+    aiPath=$"/subscriptions/$subscriptionId/resourceGroups/$resourceGroupName/providers/microsoft.insights/components/$appInsightsName"
+    body=$"{\"name\":\"$appInsightsName-ApiKey\",\"linkedReadProperties\":[\"$aiPath/api\"]}"
+    len=$(expr length $body)
+    url="https://management.azure.com$aiPath/apikeys?api-version=2015-05-01"
+    result=$(curl -X POST \
+        -H "Authorization: Bearer $token" \
+        -H "Content-Type: application/json" \
+        -H "Content-Length: $len" \
+        -s \
+        $url \
+        -d $body)
+    apiKey=$(echo $result | jq -r '.apiKey')
+    appId=$(echo $appInsightsDetails | jq -r '.properties.AppId')
+    instrumentationKey=$(echo $appInsightsDetails | jq -r '.properties.InstrumentationKey')
+
+    sed -i "s|<instrumentation-key>|$instrumentationKey|g" $gitRepoWorkingDirectory/appsettings.json
+    echo $apiKey > ~/$apiKeyTempFile
+    echo $appId > ~/$appIdTempFile
+    echo $instrumentationKey > ~/$instrumentationKeyTempFile
+
+    echo "Provisioned Azure Monitor Application Insights!"
+}
+
 # Create resources
 initEnvironment
 downloadAndBuild &
 setAzureCliDefaults
 provisionResourceGroup
 provisionDatabase &
+provisionAppInsights &
 wait &>/dev/null
 resetAzureCliDefaults
 cd $srcWorkingDirectory
-showResults
+writeResultsFile
 code .
 cd $gitRepoWorkingDirectory
+writeRememberScript
+cleanupTempFiles
+
+# We're done! Summarize.
+echo $'Done!\n\n'
+~/dotnetsay/dotnetsay $'\n\033[1;37mYour environment is ready!\n\033[0;37mI set up some \033[1;34mAzure\033[0;37m resources and downloaded the code you\'ll need.\n\033[1;35mYou can find your connection information below.\033[1;35m'
+. ~/$variableScript
