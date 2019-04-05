bc2f84f641ea77645fc661883885e5657c1d24df
Cam Soper <cam.soper@outlook.com>
2019-04-04 19:22:51 -0500

    Script enhancements
    Co-authored-by: tobias.addie@gmail.com <tobias.addie@gmail.com>

--- setup/setup.sh
+++ setup/setup.sh
@@ -1,11 +1,68 @@
 #!/bin/bash
-# If the script has already been run, just set the vars and leave.
+
+# Hi!
+# If you're reading this, you're probably interested in what's 
+# going on within this script. We've provided what we hope are useful
+# comments inline, as well as color-coded relevant shell output.
+# We hope it's useful for you, but if you have any questions
+# please open an issue on https:/github.com/MicrosoftDocs/learn-aspnet-core.
+
+# If the script appears to have already been run, just set the vars and leave.
 declare variableScript='variables.sh'
 if [ -e ~/$variableScript ]
 then
     . ~/$variableScript
     return 1
 fi
+
+# Text formatting
+declare red=`tput setaf 1`
+declare green=`tput setaf 2`
+declare yellow=`tput setaf 3`
+declare blue=`tput setaf 4`
+declare magenta=`tput setaf 5`
+declare cyan=`tput setaf 6`
+declare white=`tput setaf 7`
+declare defaultColor=`tput setaf 9`
+declare bold=`tput bold`
+declare plain=`tput sgr0`
+declare newline=$'\n'
+
+# Check to make sure we're in Azure Cloud Shell
+if [ "${AZURE_HTTP_USER_AGENT:0:11}" != "cloud-shell" ]
+then
+    echo "${bold}${red}WARNING!!!${plain}${white}" \
+        "It doesn't appear you are currently running in an instance of Azure Cloud Shell. " \
+        "Please only proceed if you know what you are doing and expected this message.${newline}${newline}" \
+        "${bold}${green}Do you know what you are doing?${plain}${white}"
+    select yn in "Yes" "No"; do
+        case $yn in
+            Yes ) break;;
+            No ) echo "${bold}${red}Please let us know that you saw this message using the feedback links provided.${plain}${white}"; return 0;;
+        esac
+    done
+fi
+
+# Figure out the name of the resource group to use
+declare resourceGroupCount=$(az group list | jq '. | length')
+declare existingResourceGroup=$(az group list | jq '.[0].name' --raw-output)
+declare defaultResourceGroupName="EfCoreModule"
+declare resourceGroupName=""
+declare rgStatus=""
+
+# If there is more than one RG or there's only one but its name is not a GUID,
+# we're probably not in the Learn sandbox.
+if [[ ! ${existingResourceGroup//-/} =~ ^[[:xdigit:]]{32}$ ]] || [ $resourceGroupCount -gt 1 ]
+then
+    echo "${bold}${red}WARNING!!!${plain}${white}" \
+        "It doesn't appear you are currently running in a Microsoft Learn sandbox." \
+        "Using default resource group."
+    resourceGroupName=$defaultResourceGroupName
+else
+    resourceGroupName=$existingResourceGroup
+fi
+
+echo "Using Azure resource group ${bold}${cyan}$resourceGroupName${plain}${white}."
 
 # Generate a random number for unique resource names
 declare instanceId=$(($RANDOM * $RANDOM))
@@ -23,7 +80,6 @@ declare sqlUsername=SqlUser
 declare sqlPassword=Pass.$RANDOM.word
 declare databaseName=ContosoPets
 declare sqlConnectionString="Data Source=$sqlServerName.database.windows.net;Initial Catalog=$databaseName;Connect Timeout=30;Encrypt=True;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False"
-declare resourceGroupName=EfCoreModule
 
 declare appInsightsName=appinsights$instanceId
 declare subscriptionId=$(az account show --query id --output tsv)
@@ -33,17 +89,7 @@ declare appIdTempFile='appId.temp'
 declare instrumentationKeyTempFile='instrumentationKey.temp'
 declare connectFile='connect.txt'
 
-declare red=`tput setaf 1`
-declare green=`tput setaf 2`
-declare yellow=`tput setaf 3`
-declare blue=`tput setaf 4`
-declare magenta=`tput setaf 5`
-declare cyan=`tput setaf 6`
-declare white=`tput setaf 7`
-declare defaultColor=`tput setaf 9`
-declare bold=`tput bold`
-declare plain=`tput sgr0`
-declare newline=$'\n'
+
 
 # Functions
 setAzureCliDefaults() {
@@ -81,7 +127,7 @@ initEnvironment(){
     # Greetings!
     greeting="${newline}${white}${bold}Hi there!${plain}${newline}"
     greeting+="I'm going to provision some ${cyan}${bold}Azure${white}${plain} resources${newline}"
-    greeting+="and get the code you'll need for this module.${magenta}"
+    greeting+="and get the code you'll need for this module.${magenta}${bold}"
 
     ~/dotnetsay/dotnetsay "$greeting"
     
@@ -100,12 +146,13 @@ downloadAndBuild() {
     echo "${white}Downloading code...${yellow}"
     (set -x; git clone --branch $gitBranch $gitUrl --quiet)
 
-    echo "${white}Building code...${magenta}"
+    echo "${white}Building code...${magenta}${bold}"
     (
         set -x 
         cd $gitRepoWorkingDirectory
         dotnet build --verbosity quiet
     ) 
+    echo "${white}${plain}"
 }
 
 # Write variables script
@@ -129,14 +176,15 @@ writeVariablesScript() {
     text+="declare appId=$(cat ~/$appIdTempFile)${newline}"
     text+="declare instrumentationKey=$(cat ~/$instrumentationKeyTempFile)${newline}"
     text+="alias db=\"sqlcmd -U $sqlUsername -P $sqlPassword -S $sqlHostName -d $databaseName\"${newline}"
-    text+="echo \"${green}${bold}Connection Info\"${newline}"
-    text+="echo \"${magenta}${bold}DB Connection String: ${white}${plain}$sqlConnectionString\"${newline}"
-    text+="echo \"${magenta}${bold}DB Host Name: ${white}${plain}$sqlHostName\"${newline}"
-    text+="echo \"${magenta}${bold}DB User Name: ${white}${plain}$sqlUsername\"${newline}"
-    text+="echo \"${magenta}${bold}DB Password: ${white}${plain}$sqlPassword\"${newline}"
-    text+="echo \"${magenta}${bold}App Insights Instrumentation Key: ${white}${plain}$(cat ~/$instrumentationKeyTempFile)\"${newline}"
-    text+="echo \"${magenta}${bold}App Insights App ID: ${white}${plain}$(cat ~/$appIdTempFile)\"${newline}"
-    text+="echo \"${magenta}${bold}App Insights API Key: ${white}${plain}$(cat ~/$apiKeyTempFile)\"${newline}"
+    text+="echo \"${green}${bold}The following variables are used in this module:\"${newline}"
+    text+="echo \"${magenta}${bold}srcWorkingDirectory: ${white}${plain}$srcWorkingDirectory\"${newline}"
+    text+="echo \"${magenta}${bold}setupWorkingDirectory: ${white}${plain}$setupWorkingDirectory\"${newline}"
+    text+="echo \"${magenta}${bold}sqlConnectionString: ${white}${plain}$sqlConnectionString\"${newline}"
+    text+="echo \"${magenta}${bold}sqlUsername: ${white}${plain}$sqlUsername\"${newline}"
+    text+="echo \"${magenta}${bold}sqlPassword: ${white}${plain}$sqlPassword\"${newline}"
+    text+="echo \"${magenta}${bold}instrumentationKey ${white}${plain}(for Application Insights)${magenta}${bold}: ${white}${plain}$(cat ~/$instrumentationKeyTempFile)\"${newline}"
+    text+="echo \"${magenta}${bold}appId ${white}${plain}(for Application Insights)${magenta}${bold}: ${white}${plain}$(cat ~/$appIdTempFile)\"${newline}"
+    text+="echo \"${magenta}${bold}apiKey ${white}${plain}(for Application Insights)${magenta}${bold}: ${white}${plain}$(cat ~/$apiKeyTempFile)\"${newline}"
     text+="echo ${newline}"
     text+="echo \"${white}db ${magenta}${bold}is an alias for${white}${plain} sqlcmd -U $sqlUsername -P $sqlPassword -S $sqlHostName -d $databaseName\"${newline}"
     text+="echo ${newline}"
@@ -155,13 +203,17 @@ cleanupTempFiles() {
 
 # Provision Azure Resource Group
 provisionResourceGroup() {
-    (
-        echo "${white}Provisioning Azure Resource Group...${cyan}"
-        set -x
-        az group create \
-            --name $resourceGroupName \
-            --output none
-    )
+    if [ $resourceGroupName = $defaultResourceGroupName ]
+    then
+        (
+            echo "${white}Provisioning Azure Resource Group...${cyan}"
+            set -x
+            az group create \
+                --name $resourceGroupName \
+                --output none
+        )
+        echo
+    fi
 }
 
 # Provision Azure SQL Database
@@ -175,6 +227,7 @@ provisionDatabase() {
             --admin-password $sqlPassword \
             --output none
     )
+    echo
     (
         echo "${white}Provisioning Azure SQL Database...${cyan}"
         set -x
@@ -183,6 +236,7 @@ provisionDatabase() {
             --server $sqlServerName \
             --output none
     )
+    echo
     (
         echo "${white}Adding Azure IP addresses to Azure SQL Database firewall rules...${cyan}"
         set -x
@@ -193,6 +247,7 @@ provisionDatabase() {
             --server $sqlServerName \
             --output none
     )
+    echo
 }
 
 provisionAppInsights() {
@@ -206,17 +261,27 @@ provisionAppInsights() {
             --properties '{"kind":"web","location":"southcentralus","properties":{"Application_Type":"web"}}' \
             --output none
     )
+    echo
 
     # Create an API Key for App Insights
     # There is no Az CLI command for this, so we must use the REST API.
-    echo "${white}Using ${cyan}${bold}Azure REST API ${white}${plain}to get App ID and API Key from Application Insights..."
-
     appInsightsDetails=$(az resource show --resource-type microsoft.insights/components --name $appInsightsName)
     token=$(az account get-access-token --output tsv --query accessToken)
     aiPath=$"/subscriptions/$subscriptionId/resourceGroups/$resourceGroupName/providers/microsoft.insights/components/$appInsightsName"
     body=$"{\"name\":\"$appInsightsName-ApiKey\",\"linkedReadProperties\":[\"$aiPath/api\"]}"
     len=$(expr length $body)
     url="https://management.azure.com$aiPath/apikeys?api-version=2015-05-01"
+
+    echo "${white}Using Azure REST API to set an API Key in Application Insights. The command looks like this (abridged for brevity):"
+    echo "${yellow}curl -X POST \\${newline}" \
+            "-H \"Authorization: Bearer <token>\" \\${newline}" \
+            "-H \"Content-Type: application/json\" \\${newline}" \
+            "-H \"Content-Length: <content length>\" \\${newline}" \
+            "-s \\${newline}" \
+            "<azure management endpoint url> \\${newline}" \
+            "-d \"{\\\"name\\\":\\\"<api key name>\\\",\\\"linkedReadProperties\\\":[\\\"<app insights resource uri>/api\\\"]}\"" \
+            "${white}"
+
     result=$(curl -X POST \
             -H "Authorization: Bearer $token" \
             -H "Content-Type: application/json" \
@@ -231,6 +296,8 @@ provisionAppInsights() {
     echo $apiKey > ~/$apiKeyTempFile
     echo $appId > ~/$appIdTempFile
     echo $instrumentationKey > ~/$instrumentationKeyTempFile
+
+    echo
 }
 
 editSettings(){
@@ -242,6 +309,7 @@ createAliases(){
     set -x
     alias db="sqlcmd -U $sqlUsername -P $sqlPassword -S $sqlHostName -d $databaseName"
     set +x
+    echo
 }
 
 # Create resources
@@ -261,7 +329,7 @@ cleanupTempFiles
 # We're done! Summarize.
 summary="${newline}${green}${bold}Your environment is ready!${white}${plain}${newline}"
 summary+="I set up some ${cyan}${bold}Azure${white}${plain} resources and downloaded the code you'll need.${newline}"
-summary+="You can display this information again by running ${red}${bold}. ~/setup.sh${white}${plain} from any location.${magenta}"
+summary+="You can display this information again by running ${red}${bold}. ~/setup.sh${white}${plain} from any location.${magenta}${bold}"
 ~/dotnetsay/dotnetsay "$summary"
 
 . ~/$variableScript
