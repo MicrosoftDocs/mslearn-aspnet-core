4e1eca60bb494b30458523c52a91a4518dd1864d
Cam Soper <cam.soper@outlook.com>
2019-05-07 15:40:12 -0500

    Modular script updates (#7)
    * Razor stuff
    
    Co-authored-by: Wade Pickett <wpickett@microsoft.com>
    
    * Starting to modularize
    
    Co-authored-by: tobias.addie@gmail.com <tobias.addie@gmail.com>
    
    * Ready for a test
    
    Co-authored-by: Scott Addie <scott.addie@microsoft.com>
    
    * testing
    
    co-authored-by: Scott Addie <scott.addie@microsoft.com>
    
    * fix
    
    * take 2
    
    co-authored-by: Scott Addie <scott.addie@microsoft.com>
    
    * fixed?
    
    co-authored-by: Scott Addie <scott.addie@microsoft.com>
    
    * refactor
    
    co-authored-by: Scott Addie <scott.addie@microsoft.com>
    
    * debugging
    
    co-authored-by: Scott Addie <scott.addie@microsoft.com>
    
    * more debugging
    
    co-authored-by: Scott Addie <scott.addie@microsoft.com>
    
    * debugging
    
    co-authored-by: Scott Addie <scott.addie@microsoft.com>
    
    * debug
    
    co-authored-by: Scott Addie <scott.addie@microsoft.com>
    
    * still broken
    
    co-authored-by: Scott Addie <scott.addie@microsoft.com>
    
    * double brackets
    
    co-authored-by: Scott Addie <scott.addie@microsoft.com>
    
    * Replace 3.0 web API with 2.2 version
    
    Co-authored-by: Cam Soper <casoper@microsoft.com>
    
    * debug
    
    co-authored-by: Scott Addie <scott.addie@microsoft.com>
    
    * more debug
    
    co-authored-by: Scott Addie <scott.addie@microsoft.com>
    
    * debug
    
    co-authored-by: Scott Addie <scott.addie@microsoft.com>
    
    * debug
    
    co-authored-by: Scott Addie <scott.addie@microsoft.com>
    
    * regex fix
    
    co-authored-by: Scott Addie <scott.addie@microsoft.com>
    
    * debug
    
    co-authored-by: Scott Addie <scott.addie@microsoft.com>
    
    * debug
    
    co-authored-by: Scott Addie <scott.addie@microsoft.com>
    
    * debug
    
    co-authored-by: Scott Addie <scott.addie@microsoft.com>
    
    * debug
    
    co-authored-by: Scott Addie <scott.addie@microsoft.com>
    
    * trying some theme changes
    
    co-authored-by: Scott Addie <scott.addie@microsoft.com>
    
    * final debugging changes
    
    co-authored-by: Scott Addie <scott.addie@microsoft.com>
    
    * AppInsights
    
    Co-authored-by: Scott Addie <scott.addie@microsoft.com>
    
    * whoops
    
    Co-authored-by: Scott Addie <scott.addie@microsoft.com>
    
    * Debugging
    
    co-authored-by: Scott Addie <scott.addie@microsoft.com>
    
    * app service
    
    co-authored-by: Scott Addie <scott.addie@microsoft.com>
    
    * debug
    
    co-authored-by: Scott Addie <scott.addie@microsoft.com>
    
    * Appservice
    
    co-authored-by: Scott Addie <scott.addie@microsoft.com>
    
    * variables should live past init script
    
    co-authored-by: Scott Addie <scott.addie@microsoft.com>
    
    * tweaks
    
    co-authored-by: Scott Addie <scott.addie@microsoft.com>
    
    * whoops
    
    * more whoops
    
    * API ready
    
    co-authored-by: Scott Addie <scott.addie@microsoft.com>
    
    * Output API URL
    
    co-authored-by: Scott Addie <scott.addie@microsoft.com>
    
    * final tweaks
    
    co-authored-by: Scott Addie <scott.addie@microsoft.com>
    
    * Git ignore location
    
    co-authored-by: Scott Addie <scott.addie@microsoft.com>
    
    * comment out code editor
    
    co-authored-by: Scott Addie <scott.addie@microsoft.com>
    
    * Adding stuff for deployment
    
    Co-authored-by: tobias.addie@gmail.com <tobias.addie@gmail.com>
    
    * fix
    
    * debugging
    
    * Switch to az webapp up
    
    Co-authored-by: tobias.addie@gmail.com <tobias.addie@gmail.com>
    
    * Not cleaning up the defaults afterall
    
    Co-Authored-By: Scott Addie <scott.addie@microsoft.com>
    
    * Mods to persist-data-ef-core script
    
    co-authored-by: Scott Addie <scott.addie@microsoft.com>
    
    * whoops
    
    co-authored-by: Scott Addie <scott.addie@microsoft.com>
    
    * More mods
    
    co-authored-by: Scott Addie <scott.addie@microsoft.com>
    
    * fix bugs
    
    co-authored-by: Scott Addie <scott.addie@microsoft.com>
    
    * whoops
    
    co-authored-by: Scott Addie <scott.addie@microsoft.com>
    
    * good to know not to do that
    
    co-authored-by: Scott Addie <scott.addie@microsoft.com>
    
    * SQL Host Name
    
    co-authored-by: Scott Addie <scott.addie@microsoft.com>
    
    * modular updates for persist-data-ef-core
    
    co-authored-by: Scott Addie <scott.addie@microsoft.com>

---
+++ infrastructure/scripts/initenvironment.sh
@@ -0,0 +1,205 @@
+# This script expects the following environment variables:
+# moduleName
+# scriptPath
+# projectRootDirectory
+
+# Common Declarations
+declare scriptPath=https://raw.githubusercontent.com/MicrosoftDocs/mslearn-aspnet-core/$gitBranch/infrastructure/scripts
+declare provisioningPath=$scriptPath/provisioning
+declare defaultLocation=southcentralus
+declare instanceId=$(($RANDOM * $RANDOM))
+declare gitDirectoriesToClone="modules/$moduleName/setup/ modules/$moduleName/src/"
+declare gitPathToCloneScript=https://raw.githubusercontent.com/MicrosoftDocs/mslearn-aspnet-core/$gitBranch/infrastructure/scripts/sparsecheckout.sh
+declare srcWorkingDirectory=~/contoso-pets/src
+declare setupWorkingDirectory=~/contoso-pets/setup
+declare subscriptionId=$(az account show --query id --output tsv)
+declare dotnetSdkVersion=$(dotnet --version)
+declare resourceGroupName=""
+
+# AppService Declarations
+declare appServicePlan=appservice$instanceId
+declare webAppName=webapp$instanceId
+
+# AppInsights Declarations
+declare appInsightsName=appinsights$instanceId
+declare apiKeyTempFile=~/.apiKey.temp
+declare appIdTempFile=~/.appId.temp
+declare instrumentationKeyTempFile=~/.instrumentationKey.temp
+
+# SQL Database Declarations
+declare sqlServerName=azsql$instanceId
+declare sqlHostName=$sqlServerName.database.windows.net
+declare sqlUsername=SqlUser
+declare sqlPassword=Pass.$RANDOM.word
+declare databaseName=ContosoPets
+declare sqlConnectionString="Data Source=$sqlHostName;Initial Catalog=$databaseName;Connect Timeout=30;Encrypt=True;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False"
+
+# Functions
+setAzureCliDefaults() {
+    echo "${headingStyle}Setting default Azure CLI values...${azCliCommandStyle}"
+    (
+        set -x
+        az configure --defaults \
+            group=$resourceGroupName \
+            location=$defaultLocation
+    )
+}
+resetAzureCliDefaults() {
+    echo "${headingStyle}Resetting default Azure CLI values...${azCliCommandStyle}"
+    (
+        set -x
+        az configure --defaults \
+            group= \
+            location=
+    )
+}
+configureDotNetCli() {
+    echo "${newline}${headingStyle}Configuring the .NET Core CLI...${defaultTextStyle}"
+
+    # By default, the .NET Core CLI prints Welcome and Telemetry messages on
+    # the first run. Suppress those messages by creating an appropriately
+    # named file on disk.
+    touch ~/.dotnet/$dotnetSdkVersion.dotnetFirstUseSentinel
+
+    # Disable the sending of telemetry to the mothership.
+    export DOTNET_CLI_TELEMETRY_OPTOUT=true
+
+    # Add ~/.dotnet/tools to the path so .NET Core Global Tool shims can be found
+    if ! [ $(echo $PATH | grep ~/.dotnet/tools) ]; then export PATH=$PATH:~/.dotnet/tools; fi
+}
+downloadAndBuild() {
+    # Set location
+    cd ~
+
+    # Set global Git config variables
+    git config --global user.name "Microsoft Learn Student"
+    git config --global user.email learn@contoso.com
+    
+    # Download the sample project, restore NuGet packages, and build
+    echo "${newline}${headingStyle}Downloading code...${defaultTextStyle}"
+    (
+        set -x
+        wget -q -O - $gitPathToCloneScript | bash -s $gitDirectoriesToClone
+    )
+    echo "${newline}${headingStyle}Building code...${defaultTextStyle}"
+    (
+        cd $srcWorkingDirectory/$projectRootDirectory
+        echo "${dotnetCliCommandStyle}"
+        set -x
+        dotnet build --verbosity quiet
+    )
+    echo "${defaultTextStyle}"
+}
+# Provision Azure SQL Database
+provisionAzSqlDatabase() {
+    declare provisionScript=$provisioningPath/azuresql.sh
+    . <(wget -q -O - $provisionScript)
+}
+# Provision App Insights
+provisionAppInsights() {
+    declare provisionScript=$provisioningPath/appinsights.sh
+    . <(wget -q -O - $provisionScript)
+}
+# Provision Azure App Service
+provisionAppService() {
+    declare provisionScript=$provisioningPath/appservice.sh
+    . <(wget -q -O - $provisionScript)
+}
+# Provision Azure Resource Group
+provisionResourceGroup() {
+    if [ "$resourceGroupName" = "$moduleName" ]; then
+        (
+            echo "${newline}${headingStyle}Provisioning Azure Resource Group...${azCliCommandStyle}"
+            set -x
+            az group create \
+                --name $resourceGroupName \
+                --output none
+        )
+    fi
+}
+addVariablesToStartup() {
+    if ! [[ $(grep $moduleName ~/.bashrc) ]]; then
+        echo "# Next line added at $(date) by $moduleName" >> ~/.bashrc
+        echo ". ~/$variableScript" >> ~/.bashrc
+    fi 
+}
+displayGreeting() {
+    # Set location
+    cd ~
+
+    # Display installed .NET Core SDK version
+    echo "${headingStyle}Using .NET Core SDK version $dotnetSdkVersion${defaultTextStyle}"
+
+    # Install .NET Core global tool to display connection info
+    dotnet tool install dotnetsay --global
+
+    # Greetings!
+    greeting="${newline}${defaultTextStyle}Hi there!${newline}"
+    greeting+="I'm going to provision some ${azCliCommandStyle}Azure${defaultTextStyle} resources${newline}"
+    greeting+="and get the code you'll need for this module.${dotnetSayStyle}"
+
+    dotnetsay "$greeting"
+}
+summarize() {
+    summary="${newline}${successStyle}Your environment is ready!${defaultTextStyle}${newline}"
+    summary+="I set up some ${azCliCommandStyle}Azure${defaultTextStyle} resources and downloaded the code you'll need.${newline}"
+    summary+="You can resume this session and display this message again by re-running the script.${dotnetSayStyle}"
+    dotnetsay "$summary"
+}
+determineResourceGroup() {
+    # Figure out the name of the resource group to use
+    declare resourceGroupCount=$(az group list | jq '. | length')
+    declare existingResourceGroup=$(az group list | jq '.[0].name' --raw-output)
+
+    # If there is more than one RG or there's only one but its name is not a GUID,
+    # we're probably not in the Learn sandbox.
+    if [[ ! ${existingResourceGroup//-/} =~ ^[[:xdigit:]]{32}$ ]] || [ $resourceGroupCount -gt 1 ]
+    then
+        echo "${warningStyle}WARNING!!!" \
+            "It appears you aren't currently running in a Microsoft Learn sandbox." \
+            "Using default resource group.${defaultTextStyle}"
+        resourceGroupName=$moduleName
+    else
+        resourceGroupName=$existingResourceGroup
+    fi
+
+    echo "Using Azure resource group ${azCliCommandStyle}$resourceGroupName${defaultTextStyle}."
+}
+checkForCloudShell() {
+    # Check to make sure we're in Azure Cloud Shell
+    if [ "${AZURE_HTTP_USER_AGENT:0:11}" != "cloud-shell" ]
+    then
+        echo "${warningStyle}WARNING!!!" \
+            "It appears you aren't running this script in an instance of Azure Cloud Shell." \
+            "This script was designed for the environment in Azure Cloud Shell, and we can make no promises that it'll function as intended anywhere else." \
+            "Please only proceed if you know what you're doing.${newline}${newline}" \
+            "Do you know what you're doing?${defaultTextStyle}"
+        select yn in "Yes" "No"; do
+            case $yn in
+                Yes ) break;;
+                No ) echo "${warningStyle}Please let us know that you saw this message using the feedback links provided.${defaultTextStyle}"; return 0;;
+            esac
+        done
+    fi
+}
+cleanupTempFiles() {
+    # App Insights
+    rm $apiKeyTempFile
+    rm $appIdTempFile
+    rm $instrumentationKeyTempFile
+}
+
+
+# Load the theme
+declare themeScript=$scriptPath/theme.sh
+. <(wget -q -O - $themeScript)
+
+# Execute functions
+checkForCloudShell
+determineResourceGroup
+configureDotNetCli
+displayGreeting
+downloadAndBuild
+setAzureCliDefaults
+
+# Additional setup in setup.sh occurs next.
---
+++ modules/create-razor-pages-aspnet-core/src/ContosoPets.Api/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using ContosoPets.Api.Data;
+
+namespace ContosoPets.Api
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = CreateWebHostBuilder(args).Build();
+            SeedDatabase(host);
+            host.Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+
+        private static void SeedDatabase(IWebHost host)
+        {
+            var scopeFactory = host.Services.GetRequiredService<IServiceScopeFactory>();
+
+            using (var scope = scopeFactory.CreateScope())
+            {
+                var context = scope.ServiceProvider.GetRequiredService<ContosoPetsContext>();
+
+                if (context.Database.EnsureCreated())
+                {
+                    try
+                    {
+                        SeedData.Initialize(context);
+                    }
+                    catch (Exception ex)
+                    {
+                        var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
+                        logger.LogError(ex, "A database seeding error occurred.");
+                    }
+                }
+            }
+        }
+    }
+}
