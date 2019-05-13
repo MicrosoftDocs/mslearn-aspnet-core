028fdc753f6833d15e966d6bf268ed20211caf5c
Cam Soper <cam.soper@outlook.com>
2019-05-13 12:41:32 -0500

    Modified to use SQLite

--- modules/create-razor-pages-aspnet-core/src/ContosoPets.Api/Startup.cs
+++ modules/create-razor-pages-aspnet-core/src/ContosoPets.Api/Startup.cs
@@ -28,7 +28,7 @@ namespace ContosoPets.Api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<ContosoPetsContext>(options =>
-                options.UseInMemoryDatabase("ContosoPets"));
+                options.UseSqlite("Data Source=contosopets.db"));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
