ce995b06a7726b23fce29a8e290c856230d09797
Cam Soper <cam.soper@outlook.com>
2019-05-14 16:09:38 -0500

    fixed

--- infrastructure/scripts/tools/sqlite3.sh
+++ infrastructure/scripts/tools/sqlite3.sh
@@ -1,15 +1,21 @@
 (
-    declare sqlitePath=~/.sqllite3
+    declare sqlitePath=~/.sqlite3
     declare sqliteUrl=https://raw.githubusercontent.com/MicrosoftDocs/mslearn-aspnet-core/$gitBranch/infrastructure/binaries/sqlite3
     echo "${newline}${headingStyle}Installing SQLite3 CLI tool...${newline}${defaultTextStyle}"
+    # Bug out if it's already installed
     if [ -d "${sqlitePath}" ]; then
         if [ -f "${sqlitePath}/sqlite3" ]; then
             echo "${warningStyle}SQLite3 CLI tool already installed."
             return 1;
         fi
     fi
+    
+    # Download and install the binary
     mkdir $sqlitePath;
-    wget -O $sqlitePath/sqlite3 $sqliteUrl
+    wget -s -O $sqlitePath/sqlite3 $sqliteUrl
+    chmod +x $sqlitePath/sqlite3
+
+    # Add the path
     if ! [ $(echo $PATH | grep $sqlitePath) ]; then 
         export PATH=$PATH:$sqlitePath
         echo "# SQLite 3 CLI tool path" >> ~/.bashrc
