801bf32c190fbd69d678c5df0e2f8f63331f1b9b
Scott Addie <scott.addie@microsoft.com>
2019-07-10 16:36:06 -0500

    Wire up Azure Key Vault
    Co-authored-by: name <Cam.Soper@outlook.com>

--- modules/secure-aspnet-core-identity/src/ContosoPets.Ui/Program.cs
+++ modules/secure-aspnet-core-identity/src/ContosoPets.Ui/Program.cs
@@ -1,5 +1,10 @@
 ﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Azure.KeyVault;
+using Microsoft.Azure.Services.AppAuthentication;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.AzureKeyVault;
+using System;
 
 namespace ContosoPets.Ui
 {
@@ -12,6 +17,20 @@ namespace ContosoPets.Ui
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration(config => ConfigureKeyVault(config))
                 .UseStartup<Startup>();
+
+        private static void ConfigureKeyVault(IConfigurationBuilder config)
+        {
+            var azureServiceTokenProvider = new AzureServiceTokenProvider();
+            var keyVaultClient = new KeyVaultClient(
+                new KeyVaultClient.AuthenticationCallback(azureServiceTokenProvider.KeyVaultTokenCallback));
+
+            string keyVaultEndpoint = Environment.GetEnvironmentVariable(
+                "ASPNETCORE_HOSTINGSTARTUP__KEYVAULT__CONFIGURATIONVAULT");
+
+            config.AddAzureKeyVault(
+                keyVaultEndpoint, keyVaultClient, new DefaultKeyVaultSecretManager());
+        }
     }
 }
