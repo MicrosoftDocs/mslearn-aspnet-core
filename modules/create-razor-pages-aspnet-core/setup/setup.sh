de6a6136f318efc256a6f93d64048239c6d76794
Cam Soper <cam.soper@outlook.com>
2019-07-24 14:50:29 -0500

    fixes

--- modules/create-razor-pages-aspnet-core/setup/setup.sh
+++ modules/create-razor-pages-aspnet-core/setup/setup.sh
@@ -14,7 +14,7 @@
 declare -x moduleName="create-razor-pages-aspnet-core"
 
 # dotnet SDK version
-declare -x dotnetSdkVersion="2.2.300"
+declare -x dotnetSdkVersion="2.2.301"
 
 # Any other declarations we need
 declare -x gitBranch="create-razor-pages-aspnet-core"
@@ -40,44 +40,10 @@ writeVariablesScript() {
     text+="declare webAppName=$webAppName${newline}"
     text+="declare webPlanName=$webPlanName${newline}"
     text+="declare webAppUrl=https://$webAppName.azurewebsites.net${newline}"
-    text+="export ASPNETCORE_HOSTINGSTARTUP__KEYVAULT__CONFIGURATIONVAULT=https://$keyVaultName.vault.azure.net${newline}"
     
-    if [ "$dbType" = "pg" ];
-    then
-        text+="declare postgreSqlServerName=$postgreSqlServerName${newline}"
-        text+="declare postgreSqlHostName=$postgreSqlHostName${newline}"
-        text+="declare postgreSqlUsername=$postgreSqlUsername@$postgreSqlServerName${newline}"
-        text+="export PGPASSWORD=$postgreSqlPassword${newline}"
-        text+="declare postgreSqlConnectionString=\"$postgreSqlConnectionString\"${newline}"
-        text+="declare postgreSqlDatabaseName=$postgreSqlDatabaseName${newline}"
-        text+="alias db=\"psql --host=$postgreSqlHostName --port=5432 --username=$postgreSqlUsername@$postgreSqlServerName --dbname=$postgreSqlDatabaseName\"${newline}"
-    else
-        text+="declare sqlServerName=$sqlServerName${newline}"
-        text+="declare sqlHostName=$sqlHostName${newline}"
-        text+="declare sqlUsername=$sqlUsername@$sqlServerName${newline}"
-        text+="declare sqlPassword=$sqlPassword${newline}"
-        text+="declare databaseName=$databaseName${newline}"
-        text+="declare sqlConnectionString=\"$sqlConnectionString\"${newline}"
-        text+="alias db=\"sqlcmd -U $sqlUsername -P $sqlPassword -S $sqlHostName -d $databaseName\"${newline}"
-    fi
-
     text+="echo \"${headingStyle}The following variables are used in this module:\"${newline}"
     text+="echo \"${headingStyle}srcWorkingDirectory: ${defaultTextStyle}$srcWorkingDirectory\"${newline}"
     text+="echo \"${headingStyle}webAppUrl: ${defaultTextStyle}$webAppUrl\"${newline}"
-    if [ "$dbType" = "pg" ];
-    then
-        text+="echo \"${headingStyle}postgreSqlConnectionString: ${defaultTextStyle}$postgreSqlConnectionString\"${newline}"
-        text+="echo \"${headingStyle}postgreSqlUsername: ${defaultTextStyle}$postgreSqlUsername\"${newline}"
-        text+="echo \"${headingStyle}PGPASSWORD: ${defaultTextStyle}$postgreSqlPassword\"${newline}"
-    text+="echo ${newline}"
-        text+="echo \"${defaultTextStyle}db ${headingStyle}is an alias for${defaultTextStyle} psql --host=$postgreSqlHostName --port=5432 --username=$postgreSqlUsername@$postgreSqlServerName --dbname=$postgreSqlDatabaseName\"${newline}"
-    else
-        text+="echo \"${headingStyle}sqlConnectionString: ${defaultTextStyle}$sqlConnectionString\"${newline}"
-        text+="echo \"${headingStyle}sqlUsername: ${defaultTextStyle}$sqlUsername\"${newline}"
-        text+="echo \"${headingStyle}sqlPassword: ${defaultTextStyle}$sqlPassword\"${newline}"
-    text+="echo ${newline}"
-        text+="echo \"${defaultTextStyle}db ${headingStyle}is an alias for${defaultTextStyle} sqlcmd -U $sqlUsername -P $sqlPassword -S $sqlHostName -d $databaseName\"${newline}"
-    fi
     text+="if ! [ \$(echo \$PATH | grep ~/.dotnet/tools) ]; then export PATH=\$PATH:~/.dotnet/tools; fi${newline}"
     text+="echo ${newline}"
     text+="cd $srcWorkingDirectory/$projectRootDirectory${newline}"
@@ -122,54 +88,8 @@ provisionAppServicePlan
     declare -x webAppName=webapp$instanceId
     declare -x projectRootDirectory="ContosoPets.Ui"
     declare -x webAppLabel="ContosoPets.Ui Web App"
-provisionAppService
-
-    ## Key Vault for UI web app
-    provisionKeyVault
-    declare -x userTemp
-    declare -x passwordTemp
-    if [ "$dbType" = "pg" ];
-    then
-        userTemp=$postgreSqlUsername && passwordTemp=$postgreSqlPassword
-    else
-        userTemp=$sqlUsername && passwordTemp=$sqlPassword
-    fi
-    
-    (
-        echo "${newline}${headingStyle}Adding database username to Azure Key Vault...${azCliCommandStyle}"
-        set -x
-        az keyvault secret set \
-            --vault-name $keyVaultName \
-            --name "DbUsername" \
-            --value "$userTemp" \
-            --output none &
-    )
-    (
-        sleep 3 # Adding a small wait to resolve a race condition
-        echo "${newline}${headingStyle}Adding database password to Azure Key Vault...${azCliCommandStyle}"
-        set -x
-        az keyvault secret set \
-            --vault-name $keyVaultName \
-            --name "DbPassword" \
-            --value "$passwordTemp" \
-            --output none 
-    )
-    (
-        echo "${newline}${headingStyle}Setting Web App environment variable with location of Key Vault...${azCliCommandStyle}"
-        set -x
-        az webapp config appsettings set \
-            --name $webAppName \
-            --settings ASPNETCORE_HOSTINGSTARTUP__KEYVAULT__CONFIGURATIONVAULT=https://$keyVaultName.vault.azure.net \
-            --output none
-    )
+    provisionAppService
 ) &
-
-if [ "$dbType" = "pg" ];
-then
-    provisionAzPostgreSqlDatabase & 
-else
-    provisionAzSqlDatabase &
-fi
 
 wait &>/dev/null
 
