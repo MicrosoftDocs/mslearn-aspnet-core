55a8d81012134400bfe3b214d4a1409e4c53ee65
Scott Addie <scott.addie@microsoft.com>
2019-08-09 10:39:58 -0500

    simplify AdminRegistrationTokenService
    Co-authored-by: Cam Soper <cam.soper@microsoft.com>

--- modules/secure-aspnet-core-identity/src/ContosoPets.Ui/Services/AdminRegistrationTokenService.cs
+++ modules/secure-aspnet-core-identity/src/ContosoPets.Ui/Services/AdminRegistrationTokenService.cs
@@ -1,50 +1,12 @@
-﻿// using ContosoPets.Ui.Areas.Identity.Data;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Threading.Tasks;
+﻿using System;
 
 namespace ContosoPets.Ui.Services
 {
     public class AdminRegistrationTokenService
     {
-        private readonly Lazy<long> _creationKey = new Lazy<long>(() => BitConverter.ToInt64(Guid.NewGuid().ToByteArray(), 7));
-        private readonly IServiceProvider _serviceProvider;
-
-        // private bool _adminExists;
-
-        public AdminRegistrationTokenService(IServiceProvider serviceProvider)
-        {
-            _serviceProvider = serviceProvider;
-        }
+        private readonly Lazy<long> _creationKey = 
+            new Lazy<long>(() => BitConverter.ToInt64(Guid.NewGuid().ToByteArray(), 7));
 
         public long CreationKey => _creationKey.Value;
-
-        // public async Task<bool> AllowAdminUserCreationAsync()
-        // {
-        //     if (_adminExists)
-        //     {
-        //         return false;
-        //     }
-        //     else
-        //     {
-        //         using (var scope = _serviceProvider.CreateScope())
-        //         {
-        //             var dbContext = scope.ServiceProvider.GetRequiredService<ContosoPetsAuth>();
-
-        //             if (await dbContext.Users.AnyAsync(user => user.IsAdmin))
-        //             {
-        //                 // There are already admin users so disable admin creation
-        //                 _adminExists = true;
-        //                 return false;
-        //             }
-
-        //             // There are no admin users so enable admin creation
-        //             return true;
-        //         }
-        //     }
-        // }
-
     }
-
 }
