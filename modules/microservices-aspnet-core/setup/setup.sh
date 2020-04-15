10f731b91581eea4beaa9399346cb539818ab3cf
Cam Soper <cam.soper@microsoft.com>
2020-04-15 12:53:36 -0500

    Moving microservices
    Co-Authored-By: Scott Addie <Scott.Addie@Microsoft.com>

---
+++ modules/microservices-aspnet-core/setup/setup.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Hi!
+# If you're reading this, you're probably interested in what's 
+# going on within this script. We've provided what we hope are useful
+# comments inline, as well as color-coded relevant shell output.
+# We hope it's useful for you, but if you have any questions or suggestions
+# please open an issue on https:/github.com/MicrosoftDocs/mslearn-aspnet-core.
+#
+
+## Start
+
+# Module name
+declare moduleName="microservices-aspnet-core"
+# dotnet SDK version
+declare -x dotnetSdkVersion="3.1.200"
+
+# Any other declarations we need
+declare -x gitBranch="microservices"
+declare initScript=https://raw.githubusercontent.com/MicrosoftDocs/mslearn-aspnet-core/$gitBranch/infrastructure/scripts/initenvironment.sh
+declare dotnetBotGreeting="I have configured .NET Core SDK $dotnetSdkVersion. Have fun!"
+declare suppressAzureResources=true
+
+# Grab and run initenvironment.sh
+. <(wget -q -O - $initScript)
