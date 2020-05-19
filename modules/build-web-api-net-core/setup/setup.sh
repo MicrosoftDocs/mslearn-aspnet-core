0e3fbb4235c4b8bbb3e0a36d343548d693bb6ac2
Scott Addie <10702007+scottaddie@users.noreply.github.com>
2020-05-19 12:50:57 -0500

    Update web API module to 3.1 (#41)
    * Update web API module to 3.1
    
    * Revert setup branch to live

--- modules/build-web-api-net-core/setup/setup.sh
+++ modules/build-web-api-net-core/setup/setup.sh
@@ -13,7 +13,7 @@
 # Module name
 declare moduleName="build-web-api-net-core"
 # dotnet SDK version
-declare -x dotnetSdkVersion="3.0.100"
+declare -x dotnetSdkVersion="3.1.202"
 
 # Any other declarations we need
 declare -x gitBranch="live"
