fa16c8c8e6c7a326a7a4497d45ecd7814ba32e0b
Cam Soper <cam.soper@microsoft.com>
2020-05-21 17:11:47 -0500

    Added cleanup-service-principal.sh

---
+++ modules/microservices-aspnet-core/src/deploy/k8s/cleanup-service-principal.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Deleting service principal(s)..."
+
+while IFS= read -r appId
+do
+    echo "az ad sp delete --id $appId"
+    az ad sp delete --id $appId
+done < <(az ad sp list --show-mine --query "[?contains(displayName,'eShop-Learn-AKS')].appId" --output tsv)
+
+echo "Done!"
