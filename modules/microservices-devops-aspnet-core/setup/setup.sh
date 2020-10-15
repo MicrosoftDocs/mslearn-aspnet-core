04f9164f6ca689cbccfc8c3ef4dea0661c4d5a20
Cam Soper <cam.soper@microsoft.com>
2020-10-15 13:14:13 -0500

    whoops

--- modules/microservices-devops-aspnet-core/setup/setup.sh
+++ modules/microservices-devops-aspnet-core/setup/setup.sh
@@ -15,10 +15,10 @@ cd ~
 declare -x dotnetSdkVersion="3.1.401"
 
 # Module name
-declare moduleName="microservices-logging-aspnet-core"
+declare moduleName="microservices-devops-aspnet-core"
 
 # Any other declarations we need
-declare -x gitBranch="microservices-logging-aspnet-core"
+declare -x gitBranch="live"
 declare initScript=https://raw.githubusercontent.com/MicrosoftDocs/mslearn-aspnet-core/$gitBranch/infrastructure/scripts/initenvironment.sh
 declare suppressAzureResources=true
 declare rootLocation=~/clouddrive
