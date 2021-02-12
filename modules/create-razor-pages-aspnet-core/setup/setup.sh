91bcd829e54ff87d9bd466a053384ef911be45f0
Scott Addie <10702007+scottaddie@users.noreply.github.com>
2021-02-12 13:46:55 -0600

    Revert Razor Pages starter app to .NET Core 3.1 (#77)
    * Revert Razor Pages starter app to .NET Core 3.1
    
    * Change SDK version
    
    * Change branch back to live

--- modules/create-razor-pages-aspnet-core/setup/setup.sh
+++ modules/create-razor-pages-aspnet-core/setup/setup.sh
@@ -14,7 +14,7 @@
 declare -x moduleName="create-razor-pages-aspnet-core"
 
 # dotnet SDK version
-declare -x dotnetSdkVersion="5.0.100"
+declare -x dotnetSdkVersion="3.1.406"
 
 # Any other declarations we need
 declare -x gitBranch="live"
--- modules/create-razor-pages-aspnet-core/src/ContosoPets.Ui/Models/Product.cs
+++ modules/create-razor-pages-aspnet-core/src/ContosoPets.Ui/Models/Product.cs
@@ -2,9 +2,14 @@
 
 namespace ContosoPets.Ui.Models
 {
-    public record Product(
-        int Id,
-        [Required] string Name,
-        [Range(0.01, 9999.99)] decimal Price
-    );
+    public class Product
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Range(0.01, 9999.99)]
+        public decimal Price { get; set; }
+    }
 }
