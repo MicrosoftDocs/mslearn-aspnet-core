04153e11f94fc3a8ddb191ec2f3edb0d4ec27e2d
Cam Soper <cam.soper@microsoft.com>
2021-05-12 15:49:21 -0500

    publish to live (#88)
    * fix the aks preview. like it says in the branch. 😊 (#83)
    
    * fix the aks preview. like it says in the branch.
    
    * setting live
    
    * Microservices data aspnet core go-live (#87)
    
    * initial commit of data module
    
    * Rearrange folder structure, add setup
    
    * merging in previous work
    
    * Fixed paths
    
    * Bumping sdk version
    
    Co-authored-by: Scott Addie <scaddie@microsoft.com>
    
    * Changes
    
    * Copying changes from vendor
    
    Co-authored-by: Scott Addie <scottaddie@users.noreply.github.com>
    Co-authored-by: David Pine <david.pine@microsoft.com>
    
    * setup change
    
    * fixed cam's whoops
    
    * revert deployment
    
    * Better script
    
    * Building ACR now
    
    * better redis resource name
    
    * Moving to eastus2
    
    * better instructions
    
    * Ready
    
    * Comment to replace
    
    * fix style
    
    * go lice
    
    Co-authored-by: Scott Addie <scaddie@microsoft.com>
    Co-authored-by: Scott Addie <scottaddie@users.noreply.github.com>
    Co-authored-by: David Pine <david.pine@microsoft.com>
    
    Co-authored-by: Scott Addie <scaddie@microsoft.com>
    Co-authored-by: Scott Addie <scottaddie@users.noreply.github.com>
    Co-authored-by: David Pine <david.pine@microsoft.com>

---
+++ modules/microservices-data-aspnet-core/src/deploy/k8s/create-azure-redis.sh
@@ -0,0 +1,84 @@
+#!/bin/bash
+
+# Color theming
+if [ -f ~/clouddrive/aspnet-learn/setup/theme.sh ]
+then
+  . <(cat ~/clouddrive/aspnet-learn/setup/theme.sh)
+fi
+
+if [ -f ~/clouddrive/aspnet-learn/create-aks-exports.txt ]
+then
+  eval $(cat ~/clouddrive/aspnet-learn/create-aks-exports.txt)
+fi
+
+if [ -f ~/clouddrive/aspnet-learn/create-idtag-exports.txt ]
+then
+  eval $(cat ~/clouddrive/aspnet-learn/create-idtag-exports.txt)
+fi
+
+if [ -z "$ESHOP_RG" ] || [ -z "$ESHOP_LOCATION" ]
+then
+    echo "One or more required environment variables are missing:"
+    echo "- ESHOP_RG.......: $ESHOP_RG"
+    echo "- ESHOP_LOCATION.: $ESHOP_LOCATION"
+    exit 1
+fi
+
+eshopIdTag=${ESHOP_IDTAG}
+
+# App Config Creation
+
+if [ -z "$eshopIdTag" ]
+then
+    dateString=$(date "+%Y%m%d%H%M%S")
+    random=`head /dev/urandom | tr -dc 0-9 | head -c 3 ; echo ''`
+
+    eshopIdTag="$dateString$random"
+fi
+
+redisName=eshop-redis-$eshopIdTag
+
+
+echo
+echo "Creating Azure Cache for Redis \"$redisName\" in resource group \"$ESHOP_RG\"..."
+acrCommand="az redis create --location $ESHOP_LOCATION --name $redisName --resource-group $ESHOP_RG --sku Basic --vm-size c0 --output none"
+echo "${newline} > ${azCliCommandStyle}$acrCommand${defaultTextStyle}${newline}"
+eval $acrCommand
+
+if [ ! $? -eq 0 ]
+then
+    echo "${newline}${errorStyle}ERROR creating Azure Cache for Redis!${defaultTextStyle}${newline}"
+    exit 1
+fi
+
+echo
+echo "Retrieving Azure Cache for Redis connection string..."
+
+primaryKey=$(az redis list-keys \
+    --resource-group $ESHOP_RG \
+    --name $redisName \
+    --query primaryKey \
+    --output tsv)
+
+if [ ! $? -eq 0 ]
+then
+    echo "ERROR!"
+    exit 1
+fi
+
+connectionString="$redisName.redis.cache.windows.net:6380,password=$primaryKey,ssl=True,abortConnect=False"
+
+echo export ESHOP_REDISNAME=$redisName >> create-azure-redis-exports.txt
+echo export ESHOP_REDISPRIMARYKEY=$primaryKey >> create-azure-redis-exports.txt
+echo export ESHOP_REDISCONNSTRING=$connectionString >> create-azure-redis-exports.txt
+echo export ESHOP_IDTAG=$eshopIdTag >> create-azure-redis-exports.txt
+
+echo export ESHOP_IDTAG=$eshopIdTag >> create-idtag-exports.txt
+
+echo "${newline}${defaultTextStyle}ConnectionString: ${headingStyle}$connectionString${defaultTextStyle}" 
+echo "${newline}${headingStyle}Done! The Azure Cache for Redis resource is provisioned, but it still has startup tasks to do. It will be a few minutes before the resource is ready.${defaultTextStyle}" 
+echo "${newline}Check the status of the resource with the following:"
+echo "${newline} > ${azCliCommandStyle}az redis show -g $ESHOP_RG -n $redisName --query provisioningState${defaultTextStyle}${newline}"
+
+mv -f create-azure-redis-exports.txt ~/clouddrive/aspnet-learn/
+mv -f create-idtag-exports.txt ~/clouddrive/aspnet-learn/
---
+++ modules/microservices-data-aspnet-core/src/src/Services/Basket/Basket.API/Infrastructure/Repositories/InMemoryBasketRepository.cs
@@ -0,0 +1,68 @@
+using Microsoft.eShopOnContainers.Services.Basket.API.Model;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Microsoft.eShopOnContainers.Services.Basket.API.Infrastructure.Repositories
+{
+    public class InMemoryBasketRepository : IBasketRepository
+    {
+        private readonly ILogger<InMemoryBasketRepository> _logger;
+        private Dictionary<string, string> _basketMapData;
+
+        public InMemoryBasketRepository(ILoggerFactory loggerFactory)
+        {
+            _logger = loggerFactory.CreateLogger<InMemoryBasketRepository>();
+            _basketMapData = new Dictionary<string, string>();
+        }
+
+        public async Task<bool> DeleteBasketAsync(string id)
+        {
+            return _basketMapData.Remove(id);
+        }
+
+        public async Task<CustomerBasket> GetBasketAsync(string customerId)
+        {
+            if (_basketMapData.ContainsKey(customerId))
+            {
+                var data = _basketMapData.GetValueOrDefault(customerId);
+
+                return JsonConvert.DeserializeObject<CustomerBasket>(data);
+            }
+
+            return null;
+        }
+
+        public IEnumerable<string> GetUsers()
+        {
+            return _basketMapData.Keys.ToList();
+        }
+
+        public async Task<CustomerBasket> UpdateBasketAsync(CustomerBasket basket)
+        {
+            if (basket == null)
+            {
+                _logger.LogInformation("Basket is empty.");
+                return null;
+            }
+
+            if (_basketMapData.ContainsKey(basket.BuyerId))
+            {
+                _logger.LogInformation("BuyerId is present. So updating the basket details.");
+                _basketMapData[basket.BuyerId] = JsonConvert.SerializeObject(basket);
+            }
+            else
+            {
+                _logger.LogInformation("BuyerId is not present. So adding it new.");
+                _basketMapData.Add(basket.BuyerId, JsonConvert.SerializeObject(basket));
+            }
+
+            _logger.LogInformation("Basket item has been captured in memory.");
+
+            return await GetBasketAsync(basket.BuyerId);
+        }
+    }
+}
