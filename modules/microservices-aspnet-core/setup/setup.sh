ff8b61beef4ed5669ed1efd127c80e9bc55ef6b9
Sumit Ghosh <sumit.ghosh@neudesic.com>
2021-09-08 14:30:16 +0530

    Updated dotnet SDK version variable to net-6

--- modules/microservices-aspnet-core/setup/setup.sh
+++ modules/microservices-aspnet-core/setup/setup.sh
@@ -12,7 +12,7 @@
 cd ~
 
 # dotnet SDK version
-declare -x dotnetSdkVersion="3.1.405"
+declare -x dotnetSdkVersion="6.0.100-preview.7"
 
 # Module name
 declare moduleName="microservices-aspnet-core"
