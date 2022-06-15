63c5d34002d3a590452e2901c1ab2509c52ee153
PLAINCONCEPTS\eduardo.cortijo <eduardo.cortijo@plainconcepts.com>
2022-06-15 09:08:16 +0200

    Update to .NET 6

--- modules/microservices-devops-aspnet-core/setup/setup.sh
+++ modules/microservices-devops-aspnet-core/setup/setup.sh
@@ -12,7 +12,7 @@
 cd ~
 
 # dotnet SDK version
-declare -x dotnetSdkVersion="3.1.401"
+declare -x dotnetSdkVersion="6.0.202"
 
 # Module name
 declare moduleName="microservices-devops-aspnet-core"
@@ -22,7 +22,7 @@ if ! [ $defaultRegion ]
 then
     declare defaultRegion=centralus
 fi
-declare -x gitBranch="live"
+declare -x gitBranch="devops-net6-latest"
 declare initScript=https://raw.githubusercontent.com/MicrosoftDocs/mslearn-aspnet-core/$gitBranch/infrastructure/scripts/initenvironment.sh
 declare suppressAzureResources=true
 declare rootLocation=~/clouddrive
