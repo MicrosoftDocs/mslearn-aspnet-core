a701aa25987903a65013341a7f6fc6f053d35c1e
PLAINCONCEPTS\eduardo.cortijo <eduardo.cortijo@plainconcepts.com>
2022-06-15 09:00:58 +0200

    Do not include apigateway module

--- modules/microservices-apigateway-aspnet-core/src/src/ApiGateways/Aggregators/Web.Sales.HttpAggregator/Models/CatalogBrand.cs
+++
@@ -1,9 +0,0 @@
-namespace Microsoft.eShopOnContainers.Web.Sales.HttpAggregator.Models
-{
-    public class CatalogBrand
-    {
-        public int Id { get; set; }
-
-        public string Brand { get; set; }
-    }
-}
