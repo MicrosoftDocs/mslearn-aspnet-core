990137cedc6581cf8df8094a9fc69576c37388c1
Cam Soper <cam.soper@microsoft.com>
2022-07-13 17:37:27 +0000

    Convert scripts

--- modules/microservices-logging-aspnet-core/deploy/k8s/quickstart.sh
+++ modules/microservices-logging-aspnet-core/deploy/k8s/quickstart.sh
@@ -1,8 +1,23 @@
 #!/bin/bash
+defaultLocation="centralus"
+defaultRg="eshop-learn-rg"
+
+# Color theming
+. <(cat ../../../../infrastructure/scripts/theme.sh)
+
+# AZ CLI check
+. <(cat ../../../../infrastructure/scripts/azure-cli-check.sh)
+
+eshopSubs=${ESHOP_SUBS}
+eshopRg=${ESHOP_RG}
+eshopLocation=${ESHOP_LOCATION}
 eshopRegistry=eshoplearn
 
 while [ "$1" != "" ]; do
     case $1 in
+        -s | --subscription)            shift
+                                        eshopSubs=$1
+                                        ;;
         -g | --resource-group)          shift
                                         eshopRg=$1
                                         ;;
@@ -15,23 +30,44 @@ while [ "$1" != "" ]; do
     shift
 done
 
+if [ -z "$eshopLocation" ]
+then
+    echo "Using the default location: $defaultLocation"
+    eshopLocation=$defaultLocation
+fi
+
 if [ -z "$eshopRg" ]
 then
-    echo "${newline}${errorStyle}ERROR: Resource group is mandatory. Use -g to set it.${defaultTextStyle}${newline}"
+    echo "Using the default resource group: $defaultRg"
+    eshopRg=$defaultRg
+fi
+echo "${bold}Note: You can change the default location and resource group by modifying the variables at the top of quickstart.sh.${defaultTextStyle}"
+
+if [ ! -z "$eshopSubs" ]
+then
+    echo "Switching to subscription $eshopSubs..."
+    az account set -s $eshopSubs
+fi
+
+if [ ! $? -eq 0 ]
+then
+    echo "${newline}${errorStyle}ERROR: Can't switch to subscription $eshopSubs.${defaultTextStyle}${newline}"
     exit 1
 fi
 
+export ESHOP_SUBS=$eshopSubs
 export ESHOP_RG=$eshopRg
 export ESHOP_LOCATION=$eshopLocation
 export ESHOP_REGISTRY=$eshopRegistry
-
-
-cd ~/clouddrive/aspnet-learn/src/deploy/k8s
+export ESHOP_QUICKSTART=true
 
 # AKS Cluster creation
+. <(cat ./create-aks.sh)
 
-./create-aks.sh
+eval $(cat ../../create-aks-exports.txt)
 
-eval $(cat ~/clouddrive/aspnet-learn/create-aks-exports.txt)
+. <(cat ./deploy-application.sh)
 
-./deploy-application.sh
+. <(cat ./create-acr.sh)
+
+cat ../../deployment-urls.txt
